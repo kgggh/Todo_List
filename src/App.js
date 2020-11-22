a037aa7a4976418c7477514d0fa394ac042833f5
kgggh <68227318+kgggh@users.noreply.github.com>
2020-11-23 00:11:11 +0900

    TodoList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  id = 3 
+
+  state ={
+    input: '',
+    todos: [
+      {id: 0, text: '자소서 수정,미니프로젝트 구상', checked: false },
+      {id: 1, text: '내 친구 종선생 운동시키기', checked: true },
+      {id: 2, text: '한숨자기', checked: false }
+    ]
+  }
+
+  handleChange =(e) => {
+    this.setState({
+      input : e.target.value
+    });
+  }
+
+  handleCreate =() => {
+    const {input, todos} = this.state;
+    this.setState({
+      input: '',
+
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false
+      })
+    });
+  }
+
+  handleKeyPress =(e) => {
+    if(e.key === 'Enter'){
+      this.handleCreate();
+    }
+  }
+
+  handleToggle = (id) => {
+    const { todos } = this.state;
+
+    const index = todos.findIndex(todo => todo.id === id);
+    const selected = todos[index];
+
+    const nextTodos = [...todos];
+
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked
+    };
+    
+    this.setState({
+      todos: nextTodos
+    })
+  }
+
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    });
+  }
+
+  render() {
+    const { input, todos } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress,
+      handleToggle,
+      handleRemove
+    } = this;
+
+    return (
+      <TodoListTemplate form={(
+        <Form
+          value={input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+          onToggle={handleToggle}
+          onRemove={handleRemove}
+        />
+      )}>        
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
+      </TodoListTemplate>
+    );
+  }
 }
 
 export default App;
